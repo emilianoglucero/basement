e18a19673d7a37dda46ac671a1dcea3ff2b0e388
Emiliano <emiliano@MacBook-Pro-de-Emiliano.local>
2021-06-26 01:32:49 -0300

    fix: delete empty components -adding minor styles to improve  pixel perfect in desktop and large screens

--- src/components/ui/SignUpImage.tsx
+++ src/components/ui/SignUpImage.tsx
@@ -1,24 +1,24 @@
-// import Image from 'next/image'
+import Image from 'next/image'
 // import styles from '../../css/SignUpImage.module.css'
 export const SignUpImage = () => {
   return (
     <>
       <section>
-        {/* <Image
-          className={styles.image}
+        <Image
+          // className={styles.image}
           src={'/images/rocket.jpeg'}
           alt="Sign up Rocket"
           // layout="intrinsic"
-          // layout="fixed"
+          layout="fixed"
           // layout="responsive"
-          layout="fill"
+          // layout="fill"
           // objectFit="cover"
           // objectFit="contain"
           // objectFit="none"
-          // width={864}
-          // height={800}
-        /> */}
-        <img src="/images/rocket.jpeg" alt="Sign up Rocket" />
+          width={864}
+          height={800}
+        />
+        {/* <img src="/images/rocket.jpeg" alt="Sign up Rocket" /> */}
       </section>
     </>
   )
